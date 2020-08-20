94e1633a062ffe6caa25c5914e95cf9f8fe3f354
ayerhs7 <62745351+ayerhs7@users.noreply.github.com>
2020-08-20 21:42:26 +0530

    Create Title.js

---
+++ src/comps/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>EduFriend</h1>
+      <h2>Your Uploads</h2>
+    </div>
+  )
+}
+
+export default Title;
